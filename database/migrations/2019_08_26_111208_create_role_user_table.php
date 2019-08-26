7b16462e64a8d8b18e8edda38cdd849e34fac333
Adam Biro <adam.biro@gmail.com>
2019-08-26 13:12:03 +0100

    omit not needed timestamp on role_user table

--- database/migrations/2019_08_26_111208_create_role_user_table.php
+++ database/migrations/2019_08_26_111208_create_role_user_table.php
@@ -19,7 +19,7 @@ class CreateRoleUserTable extends Migration
             $table->unsignedBigInteger('role_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('role_id')->references('id')->on('roles');
-            $table->timestamps();
+            //$table->timestamps();
         });
     }
     
