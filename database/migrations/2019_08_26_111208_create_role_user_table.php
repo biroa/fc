efa6428adb195136b368a9c9e6f7ce83cd12ba3c
Adam Biro <adam.biro@gmail.com>
2019-08-26 12:47:32 +0100

    add missing columns to the role_user

--- database/migrations/2019_08_26_111208_create_role_user_table.php
+++ database/migrations/2019_08_26_111208_create_role_user_table.php
@@ -15,6 +15,8 @@ class CreateRoleUserTable extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('role_id')->references('id')->on('roles');
             $table->timestamps();
