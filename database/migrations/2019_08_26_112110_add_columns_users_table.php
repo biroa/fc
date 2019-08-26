8f56273b9ebb3e3a120cfe60ba89057b2c9ec5b5
Adam Biro <adam.biro@gmail.com>
2019-08-26 13:05:49 +0100

    add unique where necessary

--- database/migrations/2019_08_26_112110_add_columns_users_table.php
+++ database/migrations/2019_08_26_112110_add_columns_users_table.php
@@ -14,7 +14,7 @@ class AddColumnsUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username')->index()->after('name');
+            $table->string('username')->unique()->after('name');
         });
     }
     
